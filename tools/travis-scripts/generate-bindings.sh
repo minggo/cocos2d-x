db4447971a050985ffe9ca958e172d35d532de53
minggo <ming@cocos2d-x.org>
2016-08-25 13:49:52 +0800

    travis build codes for linux

--- tools/travis-scripts/generate-bindings.sh
+++ tools/travis-scripts/generate-bindings.sh
@@ -62,107 +62,104 @@ generate_bindings_glue_codes()
     popd
 }
 
-if [ "$GEN_BINDING"x != "YES"x ]; then
-    generate_bindings_glue_codes
-    exit 0
-fi
-
-pushd "$PROJECT_ROOT"
-#Set git user for cocos2d-lua repo
-git config user.email ${GH_EMAIL}
-git config user.name ${GH_USER}
-popd
-
-#backup the chipmunk js bindings
-backup_dir="$JS_AUTO_GENERATED_DIR"/../backup
-echo $backup_dir
-mkdir $backup_dir
-cp "$JS_AUTO_GENERATED_DIR"/js_bindings_chipmunk_auto_classes.cpp "$backup_dir"/
-cp "$JS_AUTO_GENERATED_DIR"/js_bindings_chipmunk_auto_classes.h "$backup_dir"/
-cp "$JS_AUTO_GENERATED_DIR"/js_bindings_chipmunk_auto_classes_registration.h "$backup_dir"/
-cp "$JS_AUTO_GENERATED_DIR"/js_bindings_chipmunk_functions.cpp "$backup_dir"/
-cp "$JS_AUTO_GENERATED_DIR"/js_bindings_chipmunk_functions.h "$backup_dir"/
-cp "$JS_AUTO_GENERATED_DIR"/js_bindings_chipmunk_functions_registration.h "$backup_dir"/
-
-rm -rf "$LUA_AUTO_GENERATED_DIR"
-mkdir "$LUA_AUTO_GENERATED_DIR"
-
-rm -rf "$JS_AUTO_GENERATED_DIR"
-mkdir "$JS_AUTO_GENERATED_DIR"
-
-cp "$backup_dir"/js_bindings_chipmunk_auto_classes.cpp "$JS_AUTO_GENERATED_DIR"/
-cp "$backup_dir"/js_bindings_chipmunk_auto_classes.h "$JS_AUTO_GENERATED_DIR"/
-cp "$backup_dir"/js_bindings_chipmunk_auto_classes_registration.h "$JS_AUTO_GENERATED_DIR"/
-cp "$backup_dir"/js_bindings_chipmunk_functions.cpp "$JS_AUTO_GENERATED_DIR"/
-cp "$backup_dir"/js_bindings_chipmunk_functions.h "$JS_AUTO_GENERATED_DIR"/
-cp "$backup_dir"/js_bindings_chipmunk_functions_registration.h "$JS_AUTO_GENERATED_DIR"/
-#move the backup chipmunk js bindings to js auto directory
-
-# 1. Generate LUA bindings
 generate_bindings_glue_codes
 
-echo
-echo Bindings generated successfully
-echo
+# pushd "$PROJECT_ROOT"
+# #Set git user for cocos2d-lua repo
+# git config user.email ${GH_EMAIL}
+# git config user.name ${GH_USER}
+# popd
 
-echo
-echo Using "'$COMMITTAG'" in the commit messages
-echo
+# #backup the chipmunk js bindings
+# backup_dir="$JS_AUTO_GENERATED_DIR"/../backup
+# echo $backup_dir
+# mkdir $backup_dir
+# cp "$JS_AUTO_GENERATED_DIR"/js_bindings_chipmunk_auto_classes.cpp "$backup_dir"/
+# cp "$JS_AUTO_GENERATED_DIR"/js_bindings_chipmunk_auto_classes.h "$backup_dir"/
+# cp "$JS_AUTO_GENERATED_DIR"/js_bindings_chipmunk_auto_classes_registration.h "$backup_dir"/
+# cp "$JS_AUTO_GENERATED_DIR"/js_bindings_chipmunk_functions.cpp "$backup_dir"/
+# cp "$JS_AUTO_GENERATED_DIR"/js_bindings_chipmunk_functions.h "$backup_dir"/
+# cp "$JS_AUTO_GENERATED_DIR"/js_bindings_chipmunk_functions_registration.h "$backup_dir"/
+
+# rm -rf "$LUA_AUTO_GENERATED_DIR"
+# mkdir "$LUA_AUTO_GENERATED_DIR"
+
+# rm -rf "$JS_AUTO_GENERATED_DIR"
+# mkdir "$JS_AUTO_GENERATED_DIR"
+
+# cp "$backup_dir"/js_bindings_chipmunk_auto_classes.cpp "$JS_AUTO_GENERATED_DIR"/
+# cp "$backup_dir"/js_bindings_chipmunk_auto_classes.h "$JS_AUTO_GENERATED_DIR"/
+# cp "$backup_dir"/js_bindings_chipmunk_auto_classes_registration.h "$JS_AUTO_GENERATED_DIR"/
+# cp "$backup_dir"/js_bindings_chipmunk_functions.cpp "$JS_AUTO_GENERATED_DIR"/
+# cp "$backup_dir"/js_bindings_chipmunk_functions.h "$JS_AUTO_GENERATED_DIR"/
+# cp "$backup_dir"/js_bindings_chipmunk_functions_registration.h "$JS_AUTO_GENERATED_DIR"/
+# #move the backup chipmunk js bindings to js auto directory
+
+# # 1. Generate LUA bindings
+# generate_bindings_glue_codes
+
+# echo
+# echo Bindings generated successfully
+# echo
+
+# echo
+# echo Using "'$COMMITTAG'" in the commit messages
+# echo
 
 
-echo Using "$ELAPSEDSECS" in the branch names for pseudo-uniqueness
+# echo Using "$ELAPSEDSECS" in the branch names for pseudo-uniqueness
 
-# 2. In Bindings repo, Check if there are any files that are different from the index
+# # 2. In Bindings repo, Check if there are any files that are different from the index
 
-pushd "$PROJECT_ROOT"
+# pushd "$PROJECT_ROOT"
 
-# Run status to record the output in the log
-git status
+# # Run status to record the output in the log
+# git status
 
-echo
-echo Comparing with origin HEAD ...
-echo
+# echo
+# echo Comparing with origin HEAD ...
+# echo
 
-git fetch origin ${FETCH_REMOTE_BRANCH}
+# git fetch origin ${FETCH_REMOTE_BRANCH}
 
-# Don't exit on non-zero return value
-set +e
-git diff FETCH_HEAD --stat --exit-code ${LUA_COMMIT_PATH}
+# # Don't exit on non-zero return value
+# set +e
+# git diff FETCH_HEAD --stat --exit-code ${LUA_COMMIT_PATH}
 
-LUA_DIFF_RETVAL=$?
+# LUA_DIFF_RETVAL=$?
 
-git diff FETCH_HEAD --stat --exit-code ${JS_COMMIT_PATH}
+# git diff FETCH_HEAD --stat --exit-code ${JS_COMMIT_PATH}
 
-JS_DIFF_RETVAL=$?
-if [ $LUA_DIFF_RETVAL -eq 0 -a $JS_DIFF_RETVAL -eq 0 ]
-then
-    echo
-    echo "No differences in generated files"
-    echo "Exiting with success."
-    echo
-    exit 0
-else
-    echo
-    echo "Generated files differ from HEAD. Continuing."
-    echo
-fi
+# JS_DIFF_RETVAL=$?
+# if [ $LUA_DIFF_RETVAL -eq 0 -a $JS_DIFF_RETVAL -eq 0 ]
+# then
+#     echo
+#     echo "No differences in generated files"
+#     echo "Exiting with success."
+#     echo
+#     exit 0
+# else
+#     echo
+#     echo "Generated files differ from HEAD. Continuing."
+#     echo
+# fi
 
-# Exit on error
-set -e
+# # Exit on error
+# set -e
 
-git add -f --all "$LUA_AUTO_GENERATED_DIR"
-git add -f --all "$JS_AUTO_GENERATED_DIR"
-git checkout -b "$COCOS_BRANCH"
-git commit -m "$COMMITTAG"
+# git add -f --all "$LUA_AUTO_GENERATED_DIR"
+# git add -f --all "$JS_AUTO_GENERATED_DIR"
+# git checkout -b "$COCOS_BRANCH"
+# git commit -m "$COMMITTAG"
 
-#Set remotes
-git remote add upstream "$COCOS_ROBOT_REMOTE" 2> /dev/null > /dev/null
+# #Set remotes
+# git remote add upstream "$COCOS_ROBOT_REMOTE" 2> /dev/null > /dev/null
 
-echo "Pushing to Robot's repo ..."
-git push -fq upstream "$COCOS_BRANCH" 2> /dev/null
+# echo "Pushing to Robot's repo ..."
+# git push -fq upstream "$COCOS_BRANCH" 2> /dev/null
 
-# 7.
-echo "Sending Pull Request to base repo ..."
-curl --user "${GH_USER}:${GH_PASSWORD}" --request POST --data "{ \"title\": \"$COMMITTAG\", \"body\": \"\", \"head\": \"${GH_USER}:${COCOS_BRANCH}\", \"base\": \"${TRAVIS_BRANCH}\"}" "${PULL_REQUEST_REPO}" 2> /dev/null > /dev/null
+# # 7.
+# echo "Sending Pull Request to base repo ..."
+# curl --user "${GH_USER}:${GH_PASSWORD}" --request POST --data "{ \"title\": \"$COMMITTAG\", \"body\": \"\", \"head\": \"${GH_USER}:${COCOS_BRANCH}\", \"base\": \"${TRAVIS_BRANCH}\"}" "${PULL_REQUEST_REPO}" 2> /dev/null > /dev/null
 
-popd
+# popd
