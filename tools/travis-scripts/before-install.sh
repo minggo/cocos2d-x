ebe8b5f27248436a68b2e0416323e7671adca300
minggo <ming@cocos2d-x.org>
2016-08-29 18:48:25 +0800

    fix script

--- tools/travis-scripts/before-install.sh
+++ tools/travis-scripts/before-install.sh
@@ -35,7 +35,7 @@ function install_linux_environment()
     bash $COCOS2DX_ROOT/build/install-deps-linux.sh
 }
 
-function install_deps()
+function download_deps()
 {
     # install dpes
     pushd $COCOS2DX_ROOT
@@ -54,12 +54,11 @@ function install_android_environment()
 }
 
 # set up environment according os and target
-function install_environement()
+function install_environement_for_pull_request()
 {
     # use NDK's clang to generate binding codes
     install_android_ndk
-    
-    install_deps
+    download_deps
 
     if [ "$TRAVIS_OS_NAME" = "linux" ]; then
         if [ "$BUILD_TARGET" = "linux" ]; then
@@ -72,52 +71,21 @@ function install_environement()
     fi
 }
 
+# should generate binding codes & cocos_files.json after merging
+function install_environement_for_after_merge()
+{
+    install_android_ndk
+    download_deps
+}
 
-install_environement
+# build pull request
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    install_environement_for_pull_request
+fi
 
-
-
-# if [ "$GEN_COCOS_FILES"x = "YES"x ]; then
-#     exit 0
-# elif [ "$GEN_BINDING"x = "YES"x ]; then
-#     if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-#         exit 0
-#     fi
-#     install_android_ndk
-# elif [ "$PLATFORM"x = "linux"x ]; then
-#     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-#     sudo apt-get update
-#     sudo apt-get install gcc-4.7 g++-4.7
-#     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
-#     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 90 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
-#     g++ --version
-#     bash $COCOS2DX_ROOT/build/install-deps-linux.sh
-#     install_android_ndk
-# elif [ "$PLATFORM"x = "android"x ]; then
-#     install_android_ndk
-
-#     install_android_ndk
-# elif [ "$PLATFORM"x = "mac-ios"x ]; then
-#     if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-#         exit 0
-#     fi
-
-#     if [ "$PUSH_TO_MAC"x = "YES"x ]; then
-#         cd $COCOS2DX_ROOT
-#         git config user.email ${GH_EMAIL_MAC}
-#         git config user.name ${GH_USER_MAC}
-#         git checkout -B $TRAVIS_BRANCH
-#         cp tools/travis-scripts/travis_mac.yml ./.travis.yml
-#         git add .travis.yml
-#         cat .travis.yml
-#         git commit --amend -m "`git log -1 --pretty=%B`"
-#         git remote add travis-mac https://$GH_USER_MAC:$GH_PASSWORD_MAC@github.com/cocos-travis-mac/cocos2d-x.git
-#         git push -f travis-mac $TRAVIS_BRANCH 2> /dev/null > /dev/null
-#     else
-#         echo "xctool version: `xctool --version`"
-#         install_android_ndk
-#     fi
-# else
-#     echo "Unknown \$PLATFORM: '$PLATFORM'"
-#     exit 1
-# fi
+# run after merging
+# - make cocos robot to send PR to cocos2d-x for new binding codes
+# - generate cocos_files.json for template
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    install_environement_for_after_merge
+fi
--- tools/travis-scripts/run-script.sh
+++ tools/travis-scripts/run-script.sh
@@ -37,13 +37,88 @@ function genernate_binding_codes()
     fi
 
     # Generate binding glue codes
-    echo "Generating bindings glue codes ..."
-    cd $COCOS2DX_ROOT/tools/travis-scripts
-    ./generate-bindings.sh
-    # ./generate-cocosfiles.sh
+
+    echo "Create auto-generated luabinding glue codes."
+    pushd "$COCOS2DX_ROOT/tools/tolua"
+    ./genbindings.py
+    popd
+
+    echo "Create auto-generated jsbinding glue codes."
+    pushd "$COCOS2DX_ROOT/tools/tojs"
+    ./genbindings.py
+    popd
 }
 
-function build()
+function generate_pull_request_for_binding_codes_and_cocosfiles()
+{
+    COCOS_ROBOT_REMOTE="https://${GH_USER}:${GH_PASSWORD}@github.com/${GH_USER}/cocos2d-x.git"
+    LUA_AUTO_GENERATE_SCRIPT_PATH="$COCOS2DX_ROOT/cocos/scripting/lua-bindings/auto"
+    JS_AUTO_GENERATE_SCRIPT_PATH="$COCOS2DX_ROOT/cocos/scripting/js-bindings/auto"
+    COCOSFILE_PATH="$COCOS2DX_ROOT/templates/cocos2dx_files.json"
+    ELAPSEDSECS=`date +%s`
+    COCOS_BRANCH="update_lua_bindings_$ELAPSEDSECS"
+    COMMITTAG="[ci skip][AUTO]: updating luabinding & jsbinding & cocos_file.json automatically"
+    PULL_REQUEST_REPO="https://api.github.com/repos/cocos2d/cocos2d-x/pulls"
+
+    pushd "$COCOS2DX_ROOT"
+    #Set git user for cocos2d-lua repo
+    git config user.email ${GH_EMAIL}
+    git config user.name ${GH_USER}#Set remotes
+    git remote add upstream "$COCOS_ROBOT_REMOTE" 2> /dev/null > /dev/null
+
+    # Run status to record the output in the log
+    git status
+
+    echo
+    echo Comparing with origin HEAD ...
+    echo
+    git fetch origin "$TRAVIS_BRANCH"
+
+    # Don't exit on non-zero return value
+    set +e
+    git diff FETCH_HEAD --stat --exit-code "$LUA_AUTO_GENERATE_SCRIPT_PATH"
+    LUA_DIFF_RETVAL=$?
+
+    git diff FETCH_HEAD --stat --exit-code "$JS_AUTO_GENERATE_SCRIPT_PATH"
+    JS_DIFF_RETVAL=$?
+
+    # generate cocos_files.json and check diff
+    echo "Updates cocos_files.json"
+    ./generate-template-files.py
+    git diff FETCH_HEAD --stat --exit-code "$COCOSFILE_PATH"
+    COCOSFILE_DIFF_RETVAL=$?
+
+    if [ $LUA_DIFF_RETVAL -eq 0 && $JS_DIFF_RETVAL -eq 0 && $COCOSFILE_DIFF_RETVAL -eq 0 ]; then
+        echo
+        echo "No differences in generated files"
+        echo "Exiting with success."
+        echo
+        exit 0
+    else
+        echo
+        echo "Generated files differ from HEAD. Continuing."
+        echo
+    fi
+
+    # Exit on error
+    set -e
+
+    git add -f --all "$LUA_AUTO_GENERATE_SCRIPT_PATH"
+    git add -f --all "$JS_AUTO_GENERATE_SCRIPT_PATH"
+    git add -f --all "$COCOSFILE_PATH"
+    git checkout -b "$COCOS_BRANCH"
+    git commit -m "$COMMITTAG"
+
+    echo "Pushing to Robot's repo ..."
+    git push -fq upstream "$COCOS_BRANCH" 2> /dev/null
+
+    echo "Sending Pull Request to base repo ..."
+    curl --user "${GH_USER}:${GH_PASSWORD}" --request POST --data "{ \"title\": \"$COMMITTAG\", \"body\": \"\", \"head\": \"${GH_USER}:${COCOS_BRANCH}\", \"base\": \"${TRAVIS_BRANCH}\"}" "${PULL_REQUEST_REPO}" 2> /dev/null > /dev/null
+
+    popd
+}
+
+function run_pull_request()
 {
     # need to generate binding codes for all targets
     genernate_binding_codes
@@ -57,91 +132,51 @@ function build()
     fi
 }
 
-build
+function run_after_merge()
+{
+    # Re-generation of the javascript bindings can perform push of the new
+    # version back to github.  We don't do this for pull requests, or if
+    # GH_USER/GH_EMAIL/GH_PASSWORD environment variables are not set correctly
+    # by the encoded variables in the .travis.yml file.  (e.g. if cloned repo's
+    # want to use travis).
+    if [ -z "${GH_EMAIL}" ]; then
+        echo "GH_EMAIL not set"
+        exit 1
+    fi
+    if [ -z "${GH_USER}" ]; then
+        echo "GH_USER not set"
+        exit 1
+    fi
+    if [ -z "${GH_PASSWORD}" ]; then
+        echo "GH_USER not set"
+        exit 1
+    fi
+
+    genernate_binding_codes
+
+    cd $COCOS2DX_ROOT/tools/travis-scripts
+    ./generate-cocosfiles.sh
+
+    generate_pull_request_for_binding_codes
+}
+
+# build pull request
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    run_pull_request
+fi
+
+# run after merging
+# - make cocos robot to send PR to cocos2d-x for new binding codes
+# - generate cocos_files.json for template
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    run_after_merge
+fi
 
 
 
 
 
-# if [ "$GEN_BINDING"x = "YES"x ]; then
-#     # Re-generation of the javascript bindings can perform push of the new
-#     # version back to github.  We don't do this for pull requests, or if
-#     # GH_USER/GH_EMAIL/GH_PASSWORD environment variables are not set correctly
-#     # by the encoded variables in the .travis.yml file.  (e.g. if cloned repo's
-#     # want to use travis).
-#     if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-#         exit 0
-#     fi
-#     if [ -z "${GH_EMAIL}" ]; then
-#         echo "GH_EMAIL not set"
-#         exit 1
-#     fi
-#     if [ -z "${GH_USER}" ]; then
-#         echo "GH_USER not set"
-#         exit 1
-#     fi
-#     if [ -z "${GH_PASSWORD}" ]; then
-#         echo "GH_USER not set"
-#         exit 1
-#     fi
 
-#     cd $COCOS2DX_ROOT/tools/travis-scripts
-#     ./generate-bindings.sh $TRAVIS_BRANCH
-# elif [ "$GEN_COCOS_FILES"x = "YES"x ]; then
-#     if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-#         exit 0
-#     fi
-#     if [ -z "${GH_EMAIL}" ]; then
-#         echo "GH_EMAIL not set"
-#         exit 1
-#     fi
-#     if [ -z "${GH_USER}" ]; then
-#         echo "GH_USER not set"
-#         exit 1
-#     fi
-#     if [ -z "${GH_PASSWORD}" ]; then
-#         echo "GH_USER not set"
-#         exit 1
-#     fi
-
-#     cd $COCOS2DX_ROOT/tools/travis-scripts
-#     ./generate-cocosfiles.sh $TRAVIS_BRANCH
-# elif [ "$PLATFORM"x = "android"x ]; then
-#     export NDK_ROOT=$HOME/bin/android-ndk
-
-#     # Generate binding glue codes
-#     echo "Generating bindings glue codes ..."
-#     cd $COCOS2DX_ROOT/tools/travis-scripts
-#     ./generate-bindings.sh
-#     ./generate-cocosfiles.sh
-
-#     cd $COCOS2DX_ROOT
-
-#     # Create a directory for temporary objects
-#     mkdir android_build_objs
-
-#     PROJECTS=("cpp-empty-test" "cpp-tests" "lua-empty-test/project" "lua-tests/project")
-#     for i in ${PROJECTS[*]}; do
-#         ln -s $COCOS2DX_ROOT/android_build_objs $COCOS2DX_ROOT/tests/$i/proj.android/obj
-#     done
-
-#     # Build all samples
-#     echo "Building all samples ..."
-#     cd $COCOS2DX_ROOT/build
-#     ./android-build.py -n "NDK_BUG=0 -j10" all
-# elif [ "$PLATFORM"x = "linux"x ]; then
-#     # Generate binding glue codes
-#     echo "Generating bindings glue codes ..."
-#     cd $COCOS2DX_ROOT/tools/travis-scripts
-#     ./generate-bindings.sh
-#     ./generate-cocosfiles.sh
-
-#     echo "Building cocos2d-x"
-#     cd $COCOS2DX_ROOT/build
-#     mkdir -p linux-build
-#     cd linux-build
-#     cmake ../..
-#     make -j10
 # elif [ "$PLATFORM"x = "mac-ios"x ]; then
 #     if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 #         exit 0
