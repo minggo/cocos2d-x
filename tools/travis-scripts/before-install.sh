9d1017cc800ebc9f3b17b465ca7c7c765d8d6e4c
minggo <ming@cocos2d-x.org>
2016-09-02 10:06:41 +0800

    change for testing

--- tools/travis-scripts/before-install.sh
+++ tools/travis-scripts/before-install.sh
@@ -96,14 +96,14 @@ function install_environement_for_after_merge()
 }
 
 # build pull request
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    install_environement_for_pull_request
-fi
+# if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+#     install_environement_for_pull_request
+# fi
 
 # run after merging
 # - make cocos robot to send PR to cocos2d-x for new binding codes
 # - generate cocos_files.json for template
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     # only one job need to send PR, linux virtual machine has better performance
     if [ $TRAVIS_OS_NAME == "linux" ] && [ $GEN_BINDING_AND_COCOSFILE == "true" ]; then
         install_environement_for_after_merge
