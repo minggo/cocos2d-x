4104585d53c397a0aec1a9974e84b3de3051cb84
minggo <ming@cocos2d-x.org>
2016-10-14 10:29:51 +0800

    add missing file

---
+++ tests/cpp-empty-test/Classes/LoadingScene.h
@@ -0,0 +1,27 @@
+//
+//  LoadingScene.hpp
+//  cocos2d_tests
+//
+//  Created by minggo on 10/12/16.
+//
+//
+
+#ifndef LoadingScene_hpp
+#define LoadingScene_hpp
+
+#include "2d/CCScene.h"
+
+class LoadingScene : public cocos2d::Scene
+{
+public:
+    static LoadingScene* create(cocos2d::Scene* newScene);
+    
+    LoadingScene(cocos2d::Scene* newScene);
+    virtual void update(float dt) override;
+    
+private:
+    int _index;
+    cocos2d::Scene* _newScene;
+};
+
+#endif /* LoadingScene_hpp */
